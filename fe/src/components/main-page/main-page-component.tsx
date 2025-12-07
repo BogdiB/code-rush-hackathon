76e33a08adebbd780df68537ad7327663b47f4c5
Bogdan <107000314+BogdyPG@users.noreply.github.com>
2025-12-07 17:13:51 +0200

    frontend base

---
+++ fe/src/components/main-page/main-page-component.tsx
@@ -0,0 +1,9 @@
+import "./main-page.css";
+
+export function MainPage() {
+	return (
+		<div className="wrapper">
+			<p>something</p>
+		</div>
+	);
+}
---
+++ fe/src/constants/recipes_constants.ts
@@ -0,0 +1,1 @@
+export const TIME_TO_READ_SECONDS = 30;
